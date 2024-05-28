80882825137d027e62c90451e981644b41d57409
Caleb Lee <caleblee31101@gmail.com>
2024-05-28 10:22:07 -0500

    Added simple write test cases

--- test/PrivateerTest.cpp
+++ test/PrivateerTest.cpp
@@ -21,13 +21,12 @@
   static size_t version_capacity(std::string version_path);
   static size_t version_block_size(std::string version_path);
 */
-class WriteTest : public testing::Test {
+class PrivateerTest : public testing::Test {
   public:
     Privateer* priv = nullptr;
-    //static std::vector<size_t> random_values;
 
     static void SetUpTestSuite() {
-      //std::generate_n(std::back_inserter(random_values), 100, utility::RandomNumberBetween(0,100 - 1));
+      std::generate_n(std::back_inserter(random_values), 100, utility::RandomNumberBetween(0,100 - 1));
     }
     static void TearDownTestSuite() {
     }
@@ -39,16 +38,66 @@ class WriteTest : public testing::Test {
       delete priv;
       std::filesystem::remove_all("datastore");
     }
+
+    static std::vector<size_t> random_values;
 };
 
-TEST_F(WriteTest, Test1) {
-  size_t size_bytes = 1024*1024*1024LLU;
-  size_t* the_ints = (size_t*) priv->create(nullptr, "v0", size_bytes);
+std::vector<size_t> PrivateerTest::random_values;
+
+TEST_F(PrivateerTest, SimpleWrite) {
+  size_t size_bytes = 1024LLU;
+  size_t num_ints = size_bytes / sizeof(size_t);
+  size_t* data = (size_t*) priv->create(nullptr, "v0", size_bytes);
+
+  size_t start = 0;
+  size_t middle = num_ints / 2;
+  size_t middle_to_end = ( num_ints / 2 ) + ( num_ints / 4 );
+  size_t end = num_ints - 1;
+  data[start] = 7;
+  data[middle] = 8;
+  data[middle_to_end] = 9;
+  data[end] = 10;
+  std::cout << "written to: " << end << std::endl;
+  priv->msync();
+  delete priv;
+
+  priv = new Privateer(Privateer::OPEN, "datastore");
+  data = (size_t*) priv->open_read_only(nullptr, "v0");
+  EXPECT_EQ(data[start], 7);
+  EXPECT_EQ(data[middle], 8);
+  EXPECT_EQ(data[middle_to_end], 9);
+  EXPECT_EQ(data[end], 10);
 }
 
-TEST_F(WriteTest, Test2) {
-  size_t size_bytes = 1024*1024*1024LLU;
-  size_t* the_ints = (size_t*) priv->create(nullptr, "v0", size_bytes);
+TEST_F(PrivateerTest, SimpleDenseWrite) {
+  size_t size_bytes = 1024LLU;
+  size_t num_ints = size_bytes / sizeof(size_t);
+  size_t* data = (size_t*) priv->create(nullptr, "v0", size_bytes);
+  for (size_t i = 0; i < num_ints; i++) {
+    data[i] = i;
+  }
+  priv->msync();
+  delete priv;
+
+  priv = new Privateer(Privateer::OPEN, "datastore");
+  data = (size_t*) priv->open_read_only(nullptr, "v0");
+  for (size_t i = 0; i < num_ints; i++) {
+    EXPECT_EQ(data[i], i);
+  }
+}
+
+TEST_F(PrivateerTest, SparseWrite) {
+  size_t size_bytes = 1024LLU;
+  size_t num_ints = size_bytes / sizeof(size_t);
+  size_t* data = (size_t*) priv->create(nullptr, "v0", size_bytes);
+}
+
+TEST_F(PrivateerTest, SimpleSnapshot) {
+
+}
+
+TEST_F(PrivateerTest, IterativeSnapshot) {
+
 }
 /*
 TEST(HelloTest, basic_privateer) {
